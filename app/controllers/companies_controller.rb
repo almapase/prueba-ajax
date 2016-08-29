3b813744bb45fbdb3a7f3afb8509580bf1fc4915
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-08-29 20:20:22 -0300

    ready crud with ajax for company, poits 1, 2 y 3

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,6 +6,7 @@ class CompaniesController < ApplicationController
   # GET /companies.json
   def index
     @companies = Company.all
+    @company = Company.new
   end
 
   # GET /companies/1
@@ -31,9 +32,11 @@ class CompaniesController < ApplicationController
       if @company.save
         format.html { redirect_to @company, notice: 'Company was successfully created.' }
         format.json { render :show, status: :created, location: @company }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @company.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
@@ -45,9 +48,11 @@ class CompaniesController < ApplicationController
       if @company.update(company_params)
         format.html { redirect_to @company, notice: 'Company was successfully updated.' }
         format.json { render :show, status: :ok, location: @company }
+        format.js
       else
         format.html { render :edit }
         format.json { render json: @company.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
@@ -59,6 +64,7 @@ class CompaniesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
       format.json { head :no_content }
+      format.js
     end
   end
 
